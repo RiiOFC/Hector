8880e3a062ba5577a515e350baefcb5872f9d3f2
HectorV2 <108326286+HectorV2@users.noreply.github.com>
2022-06-29 11:37:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,10 +12,9 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
   fetch('http://kitkabackend.eastus.cloudapp.azure.com:5010/round/finishv2/3', {
     method: 'GET',
     headers: {
-      'authorization': auth
-       'use_response_compression': true
-       'Accept-Encoding': "gzip",
-       'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64))",
+      'authorization':auth
+       "Accept-Encoding": "gzip",
+       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64))",
     }
   })
     .then(res => res.text())
