fc94adfda5e089f065ee55a03e6529d6262f4a53
HectorV2 <108326286+HectorV2@users.noreply.github.com>
2022-06-30 06:14:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -28,22 +28,15 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 (async () => {
 
   console.log(chalk.yellowBright(`
-██╗  ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗ 
+██╗  ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗                                                 
 ██║  ██║██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
 ███████║█████╗  ██║        ██║   ██║   ██║██████╔╝
 ██╔══██║██╔══╝  ██║        ██║   ██║   ██║██╔══██╗
 ██║  ██║███████╗╚██████╗   ██║   ╚██████╔╝██║  ██║
-╚═╝  ╚═╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝
-                                                  
-                ██╗   ██╗██████╗                  
-                ██║   ██║╚════██╗                 
-                ██║   ██║ █████╔╝                 
-                ╚██╗ ██╔╝██╔═══╝                  
-                 ╚████╔╝ ███████╗ 
-                  ╚═══╝  ╚══════╝`));                                                                                                                                           
+╚═╝  ╚═╝╚══════╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝                                                                                                                                           
 ${chalk.bold(chalk.redBright('Made by'))} : ${chalk.bold(chalk.redBright('HectorV2'))};
 ${chalk.bold(chalk.redBright('Discord : Vicenzo#3819'))};
-
+`));
 
   const auth = rs.question(chalk.blueBright(chalk.bold('Enter Auth Token : ')));
   console.log(chalk.blueBright(chalk.bold('STARTING')));
