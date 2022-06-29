9d334f1b5127b30052e309d3e25d09ade568b28a
HectorV2 <108326286+HectorV2@users.noreply.github.com>
2022-06-30 06:07:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
 
 (async () => {
 
-  console.log(`
+  console.log(chalkYellow(`
 ██╗  ██╗███████╗ ██████╗████████╗ ██████╗ ██████╗ 
 ██║  ██║██╔════╝██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗
 ███████║█████╗  ██║        ██║   ██║   ██║██████╔╝
@@ -41,9 +41,9 @@ const GoStumble = (auth) => new Promise((resolve, reject) => {
                 ╚██╗ ██╔╝██╔═══╝                  
                  ╚████╔╝ ███████╗ 
                   ╚═══╝  ╚══════╝                                                                                                                                           
-${chalk.bold(chalk.redBright('By'))} : ${chalk.bold(chalk.redBright('HectorV2'))}
+${chalk.bold(chalk.redBright('Made by'))} : ${chalk.bold(chalk.redBright('HectorV2'))}
 ${chalk.bold(chalk.redBright('Discord : Vicenzo#3819'))}
-`);
+`));
 
   const auth = rs.question(chalk.blueBright(chalk.bold('Enter Auth Token : ')));
   console.log(chalk.blueBright(chalk.bold('STARTING')));
